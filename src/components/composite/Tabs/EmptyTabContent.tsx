6cc1f4b6381c8c6c1e10c4d4b1a8eb4700dd72b7
Mike Noired <unkstd@mail.ru>
2026-01-07 17:47:11 +0300

    add caption for empty tab

--- src/components/composite/Tabs/EmptyTabContent.tsx
+++ src/components/composite/Tabs/EmptyTabContent.tsx
@@ -15,17 +15,22 @@ export const EmptyTabContent = ({ onCreateClick }: EmptyTabContentProps) => {
   ];
 
   return (
-    <div className="h-full flex flex-col items-center justify-center p-8 bg-background">
+    <div className="h-full flex flex-col items-center justify-center p-8 bg-background gap-4">
+      <span className="text-center text-muted-foreground">
+        <span className="font-medium">Создайте новый элемент в хранилище</span>
+        <br />
+        <span className="italic">или выберите существующий из списка</span>
+      </span>
       <div className="flex gap-4 flex-wrap justify-center">
         {actions.map((action) => (
           <button
             type="button"
             key={action.type}
-            className="flex flex-col items-center justify-center gap-4 cursor-pointer rounded-md size-32 hover:bg-border border-2 border-border transition-all"
+            className="flex flex-col items-center justify-center gap-2 cursor-pointer rounded-md size-28 hover:bg-border border-2 border-border transition-all"
             onClick={() => onCreateClick(action.type)}
           >
-            <action.icon className="size-10" />
-            <span className="font-medium">{action.label}</span>
+            <action.icon className="size-8 text-muted-foreground" />
+            <span className="font-medium text-muted-foreground">{action.label}</span>
           </button>
         ))}
       </div>
